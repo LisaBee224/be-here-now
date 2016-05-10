5c1cef9dfff02e5bd389645ca87269343d692353
Lisa Buch & Esther Leytush <pair+LisaBee224.mindplace@devbootcamp.com>
2016-05-10 14:52:02 -0400

    Begin animation

---
+++ public/js/js.js
@@ -0,0 +1,25 @@
+var $animation_elements = $('.animation-element');
+var $window = $('window');
+
+function in_view(){
+  var window_height = $window.height();
+  var window_top = $window.scrollTop();
+  var window_bottom = (window_top + window_height);
+
+  $.each($animation_elements, function(){
+    var $element = $(this);
+    var $element_height = $element.outerHeight();
+    var $element_top = $element_height.offset().top();
+    var $element_bottom = ($element_height + $element_top);
+
+    if ((element_bottom >= window_top)) && ((element_top <= window_bottom)){
+      $element.addClass('in-view');
+    }
+    else{
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', in_view);
+$window.trigger('scroll');
